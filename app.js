42a948f6a5b8d58bfba2557f1c9ccc23eeaac3f6
David Orr <enki.eridu@gmail.com>
2016-09-21 09:32:06 +0000

    Pagerank optimizations incl inline angular core lib and CSS

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use('/images', express.static(__dirname + '/public/images'));
 app.use('/scripts', express.static(__dirname + '/public/scripts'));
 app.use('/styles', express.static(__dirname + '/public/styles'));
 app.use('/views', express.static(__dirname + '/public/views'));
+app.use('/nm/angular', express.static(__dirname + '/public/nm/angular'));
+app.use('/nm/angular-animate', express.static(__dirname + '/public/nm/angular-animate'));
+app.use('/nm/angular-aria', express.static(__dirname + '/public/nm/angular-aria'));
+app.use('/nm/angular-material', express.static(__dirname + '/public/nm/angular-material'));
+app.use('/nm/angular-messages', express.static(__dirname + '/public/nm/angular-messages'));
+app.use('/nm/angular-route', express.static(__dirname + '/public/nm/angular-route'));
+app.use('/nm/jquery/dist', express.static(__dirname + '/public/nm/jquery/dist'));
+
+
+
 
 app.all('/*', function(req, res, next) {
   // Just send the index.html for other files to support HTML5Mode
---
+++ public/scripts/main.js
@@ -0,0 +1,74 @@
+        console.log('provisioning dataLayer object for GTM interaction..');
+        var dataLayer = window.dataLayer = window.dataLayer || [];
+
+        console.log('initializing angular app..');
+        var app = angular.module('rebeldottie', ['ngMaterial', 'ngRoute']);
+
+        console.log('initializing coreControls..');
+        app.controller('coreControls', ['$scope', '$rootScope', '$location', '$route', '$mdSidenav',
+            function($scope, $rootScope, $location, $route, $mdSidenav) {
+                console.log('coreControls init');
+
+                /*
+                    Routing and view stuff
+                    viewTitle must be set in the view
+                    otherwise it seems to kick back an error
+                    not sure how to setup a test because
+                    even checking the undefined viewTitle
+                    causes a critical stop
+                */
+                $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+                    $scope.title = $route.current.$$route.viewTitle;
+                    dataLayer.push({
+                        event: 'ngRouteChange',
+                        attributes: {
+                            route: $location.path()
+                        }
+                    });
+                });
+
+                /*
+                    Code to toggle sideNav
+                    Also triggers a GTM dataLayer event to track views
+                */
+                $scope.toggleSideNav = function() {
+                    $mdSidenav('sideNav').toggle();
+                    dataLayer.push({
+                        event: 'sideNavToggled',
+                        attributes: {
+                            open: $mdSidenav('sideNav').isOpen()
+                        }
+                    });
+                };
+            }
+        ]);
+
+        /**
+         * 
+         * Here you should be able to define any view template
+         * Note: it must contain a viewTitle
+         *       no idea yet how to make it stop kicking errors
+         *       whenever I test if viewTitle is set already
+         * 
+         **/
+        app.config(function($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/about', {
+                    templateUrl: 'views/about.html',
+                    viewTitle: 'About'
+                })
+                .when('/contact', {
+                    templateUrl: 'views/contact.html',
+                    viewTitle: 'Contact'
+                })
+                .when('/', {
+                    templateUrl: 'views/home.html',
+                    viewTitle: 'Home'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+
+            // use the HTML5 History API
+            $locationProvider.html5Mode(true);
+        });
